eedbd11a718481335832128c534bcd744f7a0733
sweezooo <vadymetro@gmail.com>
2025-11-20 20:29:54 +0200

    aa

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
+document.querySelector('.theme input').addEventListener('change', function() {
+    document.body.classList.toggle('light-theme', this.checked);
 
+    // Эффект затемнения при смене темы
+    document.body.classList.add('theme-fade');
+    setTimeout(() => document.body.classList.remove('theme-fade'), 400);
+});
+const log = document.getElementById('log')
+
+log.addEventListener("click", function(){
+    window.location.assign('registration.html')
+})
